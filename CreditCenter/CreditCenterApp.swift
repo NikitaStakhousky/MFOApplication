cbad1e79255e7b45e3227875a229b44f87cb47c6
URKCELL\EXT02D25971 <maksim.chizhavko@consultant.turkcell.com.tr>
2023-05-11 15:19:34 +0300

    Initial Commit

---
+++ CreditCenter/CreditCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCenterApp.swift
+//  CreditCenter
+//
+//  Created by Maksim Chizhavko on 11.05.23.
+//
+
+import SwiftUI
+
+@main
+struct CreditCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
